0a53a6f98e99c3235589a6125bb70cf89a5bf48a
Philip Procházka <ph.prochazka.git@gmail.com>
2024-08-15 14:01:36 +0200

    REMOVE SOURCE FOLDERS FROM GH-PAGES BRANCH AND MANUALLY DEPLOY HTML CONTENT

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2024 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
 //
 // Scripts
 // 
